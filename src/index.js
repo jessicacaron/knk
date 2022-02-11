b44d401eb63b95dfc18707f1cae4997709b9f140
jessicacaron <77312057+jessicacaron@users.noreply.github.com>
2022-02-11 11:52:30 -0700

    init

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// JS
+import './js/'
+
+// SCSS
+import './assets/scss/main.scss'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+// CSS (example)
+// import './assets/css/main.css'
+
+
