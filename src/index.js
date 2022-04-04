0d358e9f072b79b11e19e7b16bd0307f4b58174a
jessicacaron <77312057+jessicacaron@users.noreply.github.com>
2022-04-03 20:46:30 -0600

    adding additional components

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ import './assets/scss/main.scss'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 // CSS (example)
-// import './assets/css/main.css'
+import './assets/css/main.css'
 
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,23 @@
 // Main js file
 // see more: https://github.com/vedees/webpack-template/blob/master/README.md#import-js-files
 
-console.log('its work')
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOptions = {
+    threshold: .65
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+    entries.forEach(entry => {
+       if(!entry.isIntersecting) {
+           return;
+       } else {
+           entry.target.classList.add('appear');
+           appearOnScroll.unobserve(entry.target);
+       }
+    })
+}, appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+})
